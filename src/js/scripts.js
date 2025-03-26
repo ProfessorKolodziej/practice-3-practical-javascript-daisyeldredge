0cc700447c9a33e26ca1daa3851e7d3ce0a44459
daisyeldredge <daisy@bu.edu>
2025-03-26 18:48:38 -0400

    worked on assignment

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,17 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+const myName = "Daisy";
+const myAge = 100;
+
+console.log(myAge);
+
+const toggledParagraph = document.querySelector(".show");
+console.log(toggledParagraph);
+
+toggledParagraph.classList.toggle("show");
+console.log(toggledParagraph);
+
+toggledParagraph.classList.toggle("hide");
+console.log(toggledParagraph);
